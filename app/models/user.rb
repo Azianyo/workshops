5e92c694d0cffd0957d708d56e622c0e36596e6a
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-07-22 19:38:44 +0200

    Passed user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :firstname
+  validates_presence_of :lastname
 
   has_many :reviews
   has_many :products
