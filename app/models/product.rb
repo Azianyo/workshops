68951fd64f1fbcfafab9cbf6162258ed8132800c
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2014-07-22 20:54:13 +0200

    Added average_rating method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :price
+
+  def average_rating
+    reviews.map(&:rating).reduce(:+)/reviews.count.to_f
+  end
+
   belongs_to :category
   belongs_to :user
   has_many :reviews
